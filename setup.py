e1072ab0e130bdf13f836a8c96af20c16cbf07d6
mf <mf@6efa3c87-76b7-0310-880b-b21ee7abef31>
2008-05-24 16:48:36 +0000

    Use a dictionary argument in setup() and add 'console' only if py2exe could be imported to prevent a warning.
    git-svn-id: http://repos.borg.ch/svn/svndumptool/trunk@213 6efa3c87-76b7-0310-880b-b21ee7abef31

--- setup.py
+++ setup.py
@@ -8,30 +8,33 @@
 
 from distutils.core import setup
 import svndump
-try:
-    import py2exe
-except:
-    pass
- 
-setup(
-    name='svndumptool',
-    version = svndump.__version,
-    description='Package and cmdline tool for processing Subversion dump files.',
-    license='GNU General Public License (GPL)',
-    url='http://svn.borg.ch/svndumptool/',
-    download_url='http://svn.borg.ch/svndumptool/0.5.0/svndumptool-0.5.0.tar.gz',
-    author='Martin Furter',
-    author_email = 'mf@rola.ch',
-    packages = [ 'svndump' ],
-    scripts = [ 'svndumptool.py' ],
-    console = [ 'svndumptool.py' ],
-    classifiers = [
+
+args = {
+    "name": 'svndumptool',
+    "version": svndump.__version,
+    "description": 'Package and cmdline tool for processing Subversion dump files.',
+    "license": 'GNU General Public License (GPL)',
+    "url": 'http://svn.borg.ch/svndumptool/',
+    "download_url": 'http://svn.borg.ch/svndumptool/0.5.0/svndumptool-0.5.0.tar.gz',
+    "author": 'Martin Furter',
+    "author_email": 'mf@rola.ch',
+    "packages": [ 'svndump' ],
+    "scripts": [ 'svndumptool.py' ],
+    "classifiers": [
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Programming Language :: Python',
         'Topic :: Software Development :: Version Control :: SVN',
-    ]
-)
+    ],
+}
+
+try:
+    import py2exe
+    args["console"] = [ 'svndumptool.py' ]
+except:
+    pass
+ 
+setup( **args )
 
